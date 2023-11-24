6fdae597ffde7c705cf5cd61d874f782cb9d2cd5
Mehdi <mehdi.mortazavian@gmail.com>
2023-11-24 12:24:46 +0330

    gateway post in database added to the project

--- services/gateway_service.go
+++ services/gateway_service.go
@@ -2,16 +2,28 @@ package services
 
 import (
 	"github.com/labstack/echo/v4"
+	"my-part/database"
 	"my-part/models"
+	"net/http"
 )
 
 func HandleCreateGateway(c echo.Context) error {
+	msg := make(map[string]string)
 	var gateway models.Gateway
 	err := (&echo.DefaultBinder{}).BindBody(c, &gateway)
 	if err != nil {
 		return err
 	}
 
-	//result, err := database.PostUser(user)
-	return nil
+	result, err1 := database.PostGateway(gateway)
+	if err1 != nil {
+		return err1
+	}
+	if result != http.StatusOK {
+		msg["message"] = "problem inserting gateway to database"
+		return c.JSON(result, msg)
+	}
+
+	msg["message"] = "gateway added to database successfully"
+	return c.JSON(http.StatusOK, msg)
 }
